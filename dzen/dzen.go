07e299055eb3f4a04938089f65de3fc307746b0f
Thomas NJ Shadwell <thomas.shadwell@googlemail.com>
2012-11-16 07:42:30 +0000

    initial commit

---
+++ dzen/dzen.go
@@ -0,0 +1,84 @@
+package dzen
+import(
+	"senbar/textwidth"
+	"strings"
+	"strconv"
+	"math"
+	//"fmt"
+)
+func contains(haystack []int, needle int) bool{
+	for _, hay := range haystack{
+		if hay == needle{
+			return true
+		}
+	}
+	return false
+}
+func hasSwitches(t string) int{
+	reference := make([]int, 0)
+	for i, chr := range t{
+		if chr == '^' && !contains(reference, i){
+			if t[i+1] != '^'{
+				if t[i-1] != '^'{
+					return i
+				} else {
+					reference=append(reference, i+1)
+				}
+			}
+		}
+	}
+	return -1
+}
+func Pie(radius uint8, perc float64, colourA, colourB string) string{
+	radiusString:=strconv.Itoa(int(radius))
+	degreeString:=strconv.Itoa(int(math.Floor(360*perc)))
+	return "^ib(1)^fg(" + colourA + ")^c(" + radiusString + ")^p(-" + radiusString + ")^fg(" + colourB + ")^c(" +radiusString + "-" + degreeString + ")^fg()"
+}
+func AlignRight(text string, iconWidth int, xFontName string) string{
+	/*
+	 * This function removes all special stuff from text,
+	 * takes into account 'sized special stuff' like rectangles,
+	 * and icons (if provided), then does calculations to align the text
+	 * to the right. Easy!
+	 */
+	oText := text
+	modifier :=0
+	caretPos:= hasSwitches(oText)
+	for caretPos != -1{
+		x:=oText[caretPos:]
+		if iconWidth != -1{
+			commandStart:=strings.IndexRune(x, '(')
+			command:=x[1:commandStart]
+			if command == "i"{
+				//If we encounter an icon
+				modifier+=iconWidth
+			} else if command == "r" || command == "ro"{
+				//Special clause if we encounter a rectangle
+				// -- we can calculate a rectangle's width.
+				commandEnd:=strings.IndexRune(x, ')')
+				rectangle:=x[commandStart+1:commandEnd]
+				if rectangle!= ""{
+					val, err := strconv.Atoi(strings.Split(rectangle, "x")[0])
+					if err !=nil{
+						panic(err)
+					}
+					modifier += val
+				}
+			} else if command == "c" || command =="co"{
+				commandEnd := strings.IndexRune(x, ')')
+				circle := x[commandStart+1:commandEnd]
+				if circle!=""{
+					val, err := strconv.Atoi(strings.Split(circle, "-")[0])
+					if err !=nil{
+						panic(err)
+					}
+					modifier+=val
+				}
+			}
+		}
+		x=x[strings.IndexRune(x,')')+1:]
+		oText=oText[:caretPos]+x
+		caretPos=hasSwitches(oText)
+	}
+	return "^p(_RIGHT)^p(-"+strconv.Itoa(int(textwidth.Get(xFontName, oText))+modifier)+")"+text+"^p()"
+}
---
+++ textwidth/textwidth.go
@@ -0,0 +1,95 @@
+package textwidth
+/*
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<X11/Xlib.h>
+
+typedef struct _Fnt {
+	XFontStruct *xfont;
+	XFontSet set;
+	int ascent;
+	int descent;
+	int height;
+} Fnt;
+
+Fnt font;
+Display *dpy;
+
+unsigned int
+textw(const char *text, unsigned int len) {
+	XRectangle r;
+
+	if(font.set) {
+		XmbTextExtents(font.set, text, len, NULL, &r);
+		return r.width;
+	}
+	return XTextWidth(font.xfont, text, len);
+}
+
+void
+setfont(const char *fontstr) {
+	char *def, **missing;
+	int i, n;
+
+	missing = NULL;
+	if(font.set)
+		XFreeFontSet(dpy, font.set);
+	font.set = XCreateFontSet(dpy, fontstr, &missing, &n, &def);
+	if(missing)
+		XFreeStringList(missing);
+	if(font.set) {
+		//XFontSetExtents *font_extents;
+		XFontStruct **xfonts;
+		char **font_names;
+		font.ascent = font.descent = 0;
+		//font_extents = XExtentsOfFontSet(font.set);
+		n = XFontsOfFontSet(font.set, &xfonts, &font_names);
+		for(i = 0, font.ascent = 0, font.descent = 0; i < n; i++) {
+			if(font.ascent < (*xfonts)->ascent)
+				font.ascent = (*xfonts)->ascent;
+			if(font.descent < (*xfonts)->descent)
+				font.descent = (*xfonts)->descent;
+			xfonts++;
+		}
+	}
+	else {
+		if(font.xfont)
+			XFreeFont(dpy, font.xfont);
+		font.xfont = NULL;
+		if(!(font.xfont = XLoadQueryFont(dpy, fontstr))) {
+			fprintf(stderr, "error, cannot load font: '%s'\n", fontstr);
+			exit(EXIT_FAILURE);
+		}
+		font.ascent = font.xfont->ascent;
+		font.descent = font.xfont->descent;
+	}
+	font.height = font.ascent + font.descent;
+}
+int
+prepare()
+{
+
+	dpy = XOpenDisplay(0);
+	if(!dpy) {
+		fprintf(stderr, "cannot open display\n");
+		return EXIT_FAILURE;
+	}
+
+	//setfont(myfont);
+	//printf("%u\n", textw(text, strlen(text)));
+
+	return EXIT_SUCCESS;
+}
+*/
+//#cgo LDFLAGS: -lX11
+import "C"
+
+func Get(fontName, text string) int64{
+	C.setfont(C.CString(fontName))
+	return int64(C.textw(C.CString(text), C.uint(len(text)+1)))
+}
+func init(){
+	C.prepare()
+}
+
